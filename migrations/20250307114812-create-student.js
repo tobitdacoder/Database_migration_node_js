cf391bb0d09243168d2d82087a30ee9c8e619c96
tobit bushenyula <115138001+tobitdacoder@users.noreply.github.com>
2025-03-07 11:58:06 +0000

    🚀 modification
    here we are modifying the student.js model

--- migrations/20250307114812-create-student.js
+++ migrations/20250307114812-create-student.js
@@ -1,31 +1,59 @@
-'use strict';
-/** @type {import('sequelize-cli').Migration} */
-module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Students', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      name: {
-        type: Sequelize.STRING
+// 'use strict';
+// /** @type {import('sequelize-cli').Migration} */
+// module.exports = {
+//   async up(queryInterface, Sequelize) {
+//     await queryInterface.createTable('Students', {
+//       id: {
+//         allowNull: false,
+//         autoIncrement: true,
+//         primaryKey: true,
+//         type: Sequelize.INTEGER
+//       },
+//       name: {
+//         type: Sequelize.STRING
+//       },
+//       email: {
+//         type: Sequelize.STRING
+//       },
+//       createdAt: {
+//         allowNull: false,
+//         type: Sequelize.DATE
+//       },
+//       updatedAt: {
+//         allowNull: false,
+//         type: Sequelize.DATE
+//       }
+//     });
+//   },
+//   async down(queryInterface, Sequelize) {
+//     await queryInterface.dropTable('Students');
+//   }
+// };
+
+
+
+const {Model, DataTypes, Sequelize} = require("sequelize");
+module.exports = (Sequelize) => {
+  class Student extends Models {}
+
+  Student.init(
+    {
+      name:{
+        type: DataTypes.STRING,
+        allowNull: false 
       },
       email: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
+        type: DataTypes.STRING,
         allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
+        unique: true
       }
-    });
-  },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Students');
-  }
+    },
+    {
+      sequelize,
+      modelName: "Student",
+      tableName: "Students",
+      timestamps: true
+    }
+  );
+  return Student;
 };
